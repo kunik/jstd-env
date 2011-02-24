85498581bd25fc18aa42d62f74c20d9eac3a3e8c
Taras Kunch <taras.kunch@gmail.com>
2011-02-25 00:05:38 +0200

    Added some more stuff

--- jstd.sh
+++ jstd.sh
@@ -1,20 +1,61 @@
 #!/usr/bin/env bash
 
 old_path=""
+jstd_dir=".jstd"
+script_dir="$( cd "$( dirname "$0" )" && pwd )"
+stuff_dir="$script_dir/stuff"
+
 function set_up {
-    echo $PATH
     old_path=$PATH
     export PATH="/tmp:$PATH"
-    echo $PATH
+    echo "You are in jstd env now! To exit, press Ctrl+D."
 }
 
 function tear_down {
-    echo $PATH
     export PATH=$old_path
-    echo $PATH
     echo "Bye!!!"
 }
 
-set_up
-bash
-tear_down
+function create {
+    echo "1) Creating directory for jstd stuff"
+    mkdir $jstd_dir
+
+    echo "2) Creating symlink on jstd jar"
+    ln -s $stuff_dir/JsTestDriver.jar $jstd_dir
+
+    echo "3) Creating new config"
+    cp $stuff_dir/jstd.conf $jstd_dir
+
+    echo ""
+    enter
+}
+
+function enter {
+    if [ ! -d $jstd_dir ]; then
+        echo "jstd env was not created"
+        exit 1
+    fi
+
+    set_up
+    bash
+    tear_down
+}
+
+function print_help {
+    echo "HELP!!!"
+}
+
+
+
+function main {
+    case $1 in
+        create  ) create ;;
+        enter   ) enter ;;
+        version ) echo "version" ;;
+        help    ) print_help ;;
+        *       ) print_help
+    esac
+}
+
+main $*
+exit 0
