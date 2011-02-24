89bea5e7e0bae841b62af6cb2af23b102e68cc38
Taras Kunch <taras.kunch@gmail.com>
2011-02-25 00:29:13 +0200

    Added functionality for creating env and entring it. Added help message

--- jstd.sh
+++ jstd.sh
@@ -18,20 +18,20 @@ function tear_down {
 
 function create {
     echo "1) Creating directory for jstd stuff"
-    mkdir $jstd_dir
+    mkdir ${jstd_dir}
 
     echo "2) Creating symlink on jstd jar"
-    ln -s $stuff_dir/JsTestDriver.jar $jstd_dir
+    ln -s $stuff_dir/JsTestDriver.jar ${jstd_dir}
 
     echo "3) Creating new config"
-    cp $stuff_dir/jstd.conf $jstd_dir
+    cp $stuff_dir/jstd.conf ${jstd_dir}
 
     echo ""
     enter
 }
 
 function enter {
-    if [ ! -d $jstd_dir ]; then
+    if [ ! -d ${jstd_dir} ]; then
         echo "jstd env was not created"
         exit 1
     fi
@@ -42,7 +42,16 @@ function enter {
 }
 
 function print_help {
-    echo "HELP!!!"
+    cat - <<HLP
+Jstd-env is a helper script that should help writing and running tests as much quickly and easy as possible.
+It creates ${jstd_dir} dir with jsTestDriver, it's config and other cool stuff inside your working copy and
+allows you to run tests with single 'run' command.
+
+Usage:
+    $0 create    creates ${jstd_dir} dir with virtual environment for you and enters it
+    $0 enter     enters current virtual environment
+    $0 help      prints the current message
+HLP
 }
 
 
@@ -51,7 +60,6 @@ function main {
     case $1 in
         create  ) create ;;
         enter   ) enter ;;
-        version ) echo "version" ;;
         help    ) print_help ;;
         *       ) print_help
     esac
