2607660c9b16793bfd9a642b497727c2c867faed
Taras Kunch <taras.kunch@gmail.com>
2011-02-26 15:13:27 +0200

    Implemented simple completion for run command

--- scripts/run_completion.sh
+++ scripts/run_completion.sh
@@ -1,2 +1,17 @@
-#! bash
+#  Completion for run:
+#
+#  run TestCase
+#
+_run_tests()
+{
+    local cur prev opts
 
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    opts=`grep 'TestCase' src/**.js tests/*.js | sed 's/.*TestCase\s*(\s*['"'"'"]\([^'"'"'"]*\).*/\1/'`
+
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+}
+
+complete -F _run_tests run
